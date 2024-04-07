c9e89d9ab16bd01daef57e7895687309a0ffe9a7
taiwobenedict <ajayitaiwotunta@gmail.com>
2024-04-07 01:15:07 +0100

    updated

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener('scroll', function() {
+    var textList = document.querySelectorAll('.service-item');
+    var middleOfViewport = window.innerHeight / 2;
+  
+    textList.forEach(function(text) {
+      var textPosition = text.getBoundingClientRect();
+      var distanceFromMiddle = Math.abs(textPosition.top + textPosition.height / 2 - middleOfViewport);
+      var blurAmount = distanceFromMiddle / middleOfViewport * 10; 
+      var opacity = 1 - (distanceFromMiddle / middleOfViewport);
+  
+      text.style.filter = 'blur(' + blurAmount + 'px)';
+      text.style.opacity = opacity;
+    });
+  });
+  
