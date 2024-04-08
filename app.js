f2be9956d254a1c5fc51faacec25582446d609e7
taiwobenedict <ajayitaiwotunta@gmail.com>
2024-04-08 06:23:30 +0100

    updated

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ function updateTexts() {
     const scale = distanceToMiddle < middleThreshold ? 1 : 0.5 + (0.2 * (middleThreshold / distanceToMiddle));
     const opacity = distanceToMiddle < middleThreshold ? 1 : 0 + (0.5 * (middleThreshold / distanceToMiddle));
     const blur = distanceToMiddle < middleThreshold ? 0 : 10 - (3 * (middleThreshold / distanceToMiddle));
-    
+
     // Fade out and blur the text when it's outside the middle range
     text.style.opacity = opacity;
     text.style.filter = `blur(${blur}px)`;
-    
+
     // Scale the text
     text.style.transform = `scale(${scale})`;
   });
@@ -26,7 +26,7 @@ function updateTexts() {
   requestId = requestAnimationFrame(updateTexts);
 }
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   if (!requestId) {
     requestId = requestAnimationFrame(updateTexts);
   }
@@ -36,7 +36,39 @@ window.addEventListener('scroll', function() {
 updateTexts();
 
 // Cancel animation frame when window is resized
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
   cancelAnimationFrame(requestId);
   requestId = null;
 });
+
+
+function removeLanguage () {
+  const langs = Array.from(document.querySelector('.goog-te-combo').children)
+  const nomLang = ['en', 'it', 'de', 'nl', 'fr', 'zh-CN']
+
+  langs.forEach(function(lang){
+    if( !nomLang.includes(lang.value)) {
+     lang.remove()
+      console.log(lang.value, lang.innerText)
+    }
+
+  })
+}
+
+
+// Remove language
+window.onload = function () {
+
+  setTimeout(() => {
+   removeLanguage()
+  }, 3000);
+
+  setInterval(() => {
+    removeLanguage()
+  }, 1000);
+}
+
+
+
+
+
